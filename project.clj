ed7b4e5b0c7e7e3e61b691edf0178f2d8625db61
Chris Braithwaite <chris@gasolinedigital.com>
2018-08-08 14:31:07 -0400

    add sample quil drawing

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject sparkl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot sparkl.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+            :description "SPARKL: Graphics Adventures in Clojure"
+            :url "http://example.com/FIXME"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :dependencies [[org.clojure/clojure "1.8.0"]
+                           [quil "2.7.1"]]
+            :main ^:skip-aot sparkl.core
+            :target-path "target/%s"
+            :profiles {:uberjar {:aot :all}})
--- src/sparkl/core.clj
+++ src/sparkl/core.clj
@@ -1,7 +1,28 @@
 (ns sparkl.core
-  (:gen-class))
+  (:require [quil.core :as q :include-macros true]))
+
+(defn setup []
+  (q/frame-rate 60)                    ;; Set framerate to 1 FPS
+  (q/background 200))                 ;; Set the background colour to
+                                      ;; a nice shade of grey.
+(defn draw []
+  (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
+  (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
+  (q/fill (q/random 255))               ;; Set the fill colour to a random grey
+
+  (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
+        x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
+        y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
+    (q/ellipse x y diam diam)))         ;; Draw a circle at x y with the correct diameter
+
+(q/defsketch example                  ;; Define a new sketch named example
+             :title "Oh so many grey circles"    ;; Set the title of the sketch
+             :settings #(q/smooth 2)             ;; Turn on anti-aliasing
+             :setup setup                        ;; Specify the setup fn
+             :draw draw                          ;; Specify the draw fn
+             :size [323 200])                    ;; You struggle to beat the golden ratio
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "See window."))
