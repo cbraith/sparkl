882c5d17d7fc05aa7e498c77cbd0f42ab99dc16e
Chris Braithwaite <chris@gasolinedigital.com>
2018-08-08 18:15:10 -0400

    set draw space

--- src/sparkl/core.clj
+++ src/sparkl/core.clj
@@ -2,25 +2,22 @@
   (:require [quil.core :as q :include-macros true]))
 
 (defn setup []
-  (q/frame-rate 60)                    ;; Set framerate to 1 FPS
-  (q/background 200))                 ;; Set the background colour to
+  (q/frame-rate 30)                    ;; Set framerate to 1 FPS
+  (q/background 0 0 0))                 ;; Set the background colour to
                                       ;; a nice shade of grey.
 (defn draw []
   (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
   (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
   (q/fill (q/random 255))               ;; Set the fill colour to a random grey
 
-  (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
-        x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
-        y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
-    (q/ellipse x y diam diam)))         ;; Draw a circle at x y with the correct diameter
+  (q/set-pixel 256 256 (q/color 255 255 255)))         ;; Draw a point at the center of the screen
 
 (q/defsketch example                  ;; Define a new sketch named example
-             :title "Oh so many grey circles"    ;; Set the title of the sketch
+             :title "A solitary point."    ;; Set the title of the sketch
              :settings #(q/smooth 2)             ;; Turn on anti-aliasing
              :setup setup                        ;; Specify the setup fn
              :draw draw                          ;; Specify the draw fn
-             :size [323 200])                    ;; You struggle to beat the golden ratio
+             :size [512 512])                    ;; You struggle to beat the golden ratio
 
 (defn -main
   "I don't do a whole lot ... yet."
