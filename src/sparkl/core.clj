8a7b8c8980ce40044ff4e5e266f36ace089291d9
Chris Braithwaite <chris@gasolinedigital.com>
2018-08-10 10:45:27 -0400

    plot set of points

--- src/sparkl/core.clj
+++ src/sparkl/core.clj
@@ -1,6 +1,19 @@
 (ns sparkl.core
   (:require [quil.core :as q :include-macros true]))
 
+(defn circle
+  [r x]
+  (Math/sqrt (- (Math/pow r 2) (Math/pow x 2))))
+
+(defn plot
+  [x y r c]
+  ;; draw a circle
+  (let [xs (range r)]
+    (map #([[(+ x %) (+ y (circle r %)) c]
+            [(+ x %) (- y (circle r %)) c]
+            [(- x %) (+ y (circle r %)) c]
+            [(- x %) (- y (circle r %)) c]]) xs)))
+
 (defn setup []
   (q/frame-rate 30)                    ;; Set framerate to 1 FPS
   (q/background 0 0 0))                 ;; Set the background colour to
@@ -10,7 +23,9 @@
   (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
   (q/fill (q/random 255))               ;; Set the fill colour to a random grey
 
-  (q/set-pixel 256 256 (q/color 255 255 255)))         ;; Draw a point at the center of the screen
+  (let [white (q/color 255 255 255)]
+    ; (q/set-pixel 256 256 white)))
+    (doseq [y (range 246 267)] (q/set-pixel 256 y white))))         ;; Draw a point at the center of the screen
 
 (q/defsketch example                  ;; Define a new sketch named example
              :title "A solitary point."    ;; Set the title of the sketch
@@ -22,4 +37,4 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "See window."))
+  (println "Hi."))
